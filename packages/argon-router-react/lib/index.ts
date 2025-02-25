4a37ce28c4de8f3c8e0c5c9b3541196c8e165135
movpushmov <gigolaev.edward@gmail.com>
2025-02-26 00:59:21 +0300

    fix: lazy route export

--- packages/argon-router-react/lib/index.ts
+++ packages/argon-router-react/lib/index.ts
@@ -2,4 +2,5 @@ export { RouterProvider } from './router-provider';
 export { Link } from './link';
 export { createRouteView } from './create-route-view';
 export { createRoutesView } from './create-routes-view';
+export { createLazyRouteView } from './create-lazy-route-view';
 export { useRouter } from './use-router';
